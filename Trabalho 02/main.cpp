ae718cfd1d6b28a7a550833001eb6bfe47526cdc
karinamaria <karinamaria163@gmail.com>
2020-09-15 18:23:01 -0300

    q07

--- Trabalho 02/main.cpp
+++ Trabalho 02/main.cpp
@@ -13,6 +13,14 @@ void printArray(int *first, int *last){
 	std::cout << "]\n";
 }
 
+void printArray(int A[], int inicio, int fim){
+	std::cout << "A [ ";
+	for ( int i {0} ; i < (fim-inicio) ; ++ i ){
+		std::cout << A[i] << " ";
+	}
+	std::cout << "]\n";
+}
+
 void printMatriz(const int (&A) [SIZE][SIZE]){
 	for (const auto & row : A){
 		std::cout << "[";
@@ -25,7 +33,7 @@ void printMatriz(const int (&A) [SIZE][SIZE]){
 
 void imprimirResultado(int *result){
 	if(result == nullptr){
-		std::cout << "\n:::::::: ARRAY VAZIO ::::::::" << std::endl;
+		std::cout << "\n:::::::: ARRAY VAZIO OU NADA FOI ENCONTRADO ::::::::" << std::endl;
 	}else{
 		std::cout << "\n-------- RESULTADO: " << *result << " --------" << std::endl;
 	}
@@ -60,11 +68,32 @@ int* run_q02(int *first, int *last){
 	return nullptr;
 }
 
+
+int* run_q07(int array[], int inicio, int fim){
+	printArray(array, inicio, fim);
+	int i;
+	
+	while(inicio < fim){
+		i = (inicio + fim)/2;
+		if(array[i] == i){
+			return &array[i];
+		}
+		if(array[i] > i){
+			fim=i;
+		}else{
+			inicio=i+1;
+		}
+	}
+	return nullptr;
+}
+
 int main(){
 	srand(time(NULL));
-	size_t numeroElementos = rand() % 10;
+	size_t numeroElementos = 5;
+	//size_t numeroElementos = rand() % 10;
 
-	int array[numeroElementos];
+	int array[numeroElementos]{0,2,9,11,16};
+	
 	int matriz[][SIZE]
 	{
 		{10, 18, 27},
@@ -73,12 +102,13 @@ int main(){
 	};
 	size_t array_sz{sizeof(array)/sizeof(array[0])};
 
-	for(size_t i=0; i<numeroElementos; i++){
+	/*for(size_t i=0; i<numeroElementos; i++){
 		array[i]=(rand() % 101);
-	}
+	}*/
 	
 
 	//imprimirResultado(run_q01(array, &array[array_sz]));
-	printMatriz(matriz);
+	imprimirResultado(run_q07(array, 0, array_sz));
+	//printMatriz(matriz);
 	return 0;
 }
