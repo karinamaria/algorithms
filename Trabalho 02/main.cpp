34a678d1c897f3061581b9f68859ef85d5b36569
karinamaria <karinamaria163@gmail.com>
2020-09-15 19:41:28 -0300

    Questão 04

--- Trabalho 02/main.cpp
+++ Trabalho 02/main.cpp
@@ -31,10 +31,11 @@ void printMatriz(const int (&A) [SIZE][SIZE]){
 	}
 }
 
-void imprimirResultado(int *result){
+void imprimirResultado(int *array, int *result){
 	if(result == nullptr){
 		std::cout << "\n:::::::: ARRAY VAZIO OU NADA FOI ENCONTRADO ::::::::" << std::endl;
 	}else{
+		std::cout << "\n-------- INDICE: " << std::distance(array, result) << " --------" << std::endl;
 		std::cout << "\n-------- RESULTADO: " << *result << " --------" << std::endl;
 	}
 }
@@ -68,6 +69,22 @@ int* run_q02(int *first, int *last){
 	return nullptr;
 }
 
+int* run_q04(int *first, int *last, int value ){
+	printArray(first, last);
+	int step, *mid;
+	int count = last-first;
+	while(count != 0){
+	   	step = count/2;
+        mid = first + step;
+        if(*mid >= value) 
+            count = step;
+        else{
+            first = mid+1;
+            count -= (step+1);
+        }
+    }
+    return first;
+}
 
 int* run_q07(int array[], int inicio, int fim){
 	printArray(array, inicio, fim);
@@ -87,12 +104,29 @@ int* run_q07(int array[], int inicio, int fim){
 	return nullptr;
 }
 
+void q04(){
+	size_t numeroElementos = 5;
+
+	int array[numeroElementos]{0,1,1,2,4};
+
+	size_t array_sz{sizeof(array)/sizeof(array[0])};
+
+	int *result = run_q04(array, &array[array_sz],1);
+	
+	if(result == &array[array_sz]){
+		std::cout << "-------- A CHAVE NAO FOI ENCONTRADA --------\n";
+	}else{
+		std::cout << "\n-------- INDICE: " << std::distance(array, result) << " --------" << std::endl;
+		std::cout << "\n-------- RESULTADO: " << *result << " --------" << std::endl;
+	}
+}
+
 int main(){
 	srand(time(NULL));
 	size_t numeroElementos = 5;
 	//size_t numeroElementos = rand() % 10;
 
-	int array[numeroElementos]{0,2,9,11,16};
+	int array[numeroElementos]{0,1,1,2,4};
 	
 	int matriz[][SIZE]
 	{
@@ -108,7 +142,8 @@ int main(){
 	
 
 	//imprimirResultado(run_q01(array, &array[array_sz]));
-	imprimirResultado(run_q07(array, 0, array_sz));
+	//imprimirResultado(array, run_q07(array, 0, array_sz));
+	q04();
 	//printMatriz(matriz);
 	return 0;
 }
