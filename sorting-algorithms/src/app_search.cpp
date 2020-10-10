271f86bb7da27ec019a33da7c1423f71df3165bb
Karina Maria <karina.jacinto@imd.ufrn.br>
2020-10-10 10:21:17 -0300

    Melhorias nas funções.

--- sorting-algorithms/src/app_search.cpp
+++ sorting-algorithms/src/app_search.cpp
@@ -9,32 +9,41 @@
 
 using namespace sa;
 
-std::string print( value_type *first, value_type *last ){
+typedef void (*fuctions_sorting) (value_type *first, value_type *last);
+
+void print( value_type *first, value_type *last ){
     std::ostringstream oss;
     std::copy( first, last, std::ostream_iterator< value_type > ( oss, " " ) );
-    return oss.str();
+    std::cout << oss.str() << std::endl;
 }
 
 void run_insertion_sort(value_type *first, value_type *last){
+	std::cout << "Insertion sort" << std::endl;
 	insertionsort(first, last);
-	std::cout << print(first, last) << std::endl;
+	print(first, last);
 }
 
 void run_bubble_sort(value_type *first, value_type *last){
+	std::cout << "Bubble sort" << std::endl;
 	bubblesort(first, last);
-	std::cout << print(first, last) << std::endl;
+	print(first, last);
 }
 
 void run_selection_sort(value_type *first, value_type *last){
+	std::cout << "Selection sort" << std::endl;
 	selectionsort(first, last);
-	std::cout << print(first, last) << std::endl;
+	print(first, last);
 }
 
 int main(){
+	fuctions_sorting functions[]{run_insertion_sort, run_bubble_sort, run_selection_sort};
 	value_type A[]{1006,23,45,293,12,43,1,49};
 
-	run_insertion_sort(std::begin(A), std::end(A));
+	//run_insertion_sort(std::begin(A), std::end(A));
 	//run_bubble_sort(std::begin(A), std::end(A));
 	//run_selection_sort(std::begin(A), std::end(A));
+	for(auto i=0; i<3; i++){
+		functions[i](std::begin(A), std::end(A));
+	}
 	return 0;
 }
